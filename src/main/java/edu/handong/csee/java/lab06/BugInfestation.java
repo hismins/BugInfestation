1c48fb9b1f753166865635be15e0caa53a3a4429
Sangmin Han <dhkgn56@gmail.com>
2018-03-31 21:13:33 +0900

    Add local variables

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -11,33 +11,4 @@ public class BugInfestation {
 		BugInfestation myBugInfestation= new BugInfestation();
 		myBugInfestation.computeNumberOfBugsToUseFKiller();
 	}	
-	public void computeNumberOfBugsToUseFKiller() {
-		Scanner keepWork = new Scanner(System.in);
-
-		System.out.print("Enter the total volume of you house\n"
-				+ "in cubic feet: ");
-		double houseVolume = keepWork.nextDouble(); //volume is might double. so scan in double.
-		System.out.print("Enter the estimated number of\n"
-				+ "roaches in your house: ");
-		int startPopulation = keepWork.nextInt();
-
-		double population = startPopulation; 
-		double totalBugVolume = population * ONE_BUG_VOLUME;
-
-		int countWeek = 0;
-
-		while (totalBugVolume < houseVolume) {
-			double newBugs = population * GROWTH_RATE; //GROWTH_RATE is double.
-			double newBugVolume = newBugs * ONE_BUG_VOLUME;
-			population = population + newBugs;
-			totalBugVolume = population * ONE_BUG_VOLUME;
-			countWeek = countWeek + 1;
-		}
-		System.out.println("Starting with a roach population of " + startPopulation);
-				System.out.println("and a houte with a volume of= " + houseVolume + " cubic feet,");
-				System.out.println("after " + countWeek+ " weeks,");
-				System.out.println("the house will be filled with " + (int)population + " roaches.");//print "population" in int type.
-				System.out.println("They will fill a volume of " + (int)totalBugVolume +" cubic feet.");
-				System.out.println("Better call Debugging Experts Inc.");
-	}
-}
+}	
